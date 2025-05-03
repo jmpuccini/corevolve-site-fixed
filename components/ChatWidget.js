4c5a7088132c43cee10bbc83a13dc1280debbdc5
Juan Manuel Mendoza <hojasdevida1978@gmail.com>
2025-05-03 00:12:04 -0400

    Final fix for chatbot hydration issue

--- components/ChatWidget.js
+++ components/ChatWidget.js
@@ -2,13 +2,14 @@ import { useEffect } from 'react';
 
 const ChatWidget = () => {
   useEffect(() => {
-    const s1 = document.createElement('script');
-    s1.src = 'https://embed.tawk.to/68158d2ffc50e9190eb83df0/1iqa2v07a';
-    s1.async = true;
-    s1.charset = 'UTF-8';
-    s1.setAttribute('crossorigin', '*');
-
-    document.body.appendChild(s1);
+    if (typeof window !== 'undefined') {
+      const s1 = document.createElement('script');
+      s1.src = 'https://embed.tawk.to/68158d2ffc50e9190eb83df0/1iqa2v07a';
+      s1.async = true;
+      s1.charset = 'UTF-8';
+      s1.setAttribute('crossorigin', '*');
+      document.body.appendChild(s1);
+    }
   }, []);
 
   return null;
