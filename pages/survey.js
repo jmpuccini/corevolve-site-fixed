a178876843e7d00aba19e73184a9f9ff2193edff
Juan Manuel Mendoza <hojasdevida1978@gmail.com>
2025-05-03 09:56:00 -0400

    Add embedded survey page

---
+++ pages/survey.js
@@ -0,0 +1,18 @@
+export default function SurveyPage() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Client Discovery Survey</h1>
+      <iframe
+        src="<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfypj-fj6lUjWK-AOLLgpGyvqRuInGiqWqHkQr9GOzbIkN8Ng/viewform?embedded=true" width="640" height="1485" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>"
+        width="100%"
+        height="800"
+        frameBorder="0"
+        marginHeight="0"
+        marginWidth="0"
+        title="Corevolve Survey"
+      >
+        Loading…
+      </iframe>
+    </div>
+  );
+}
