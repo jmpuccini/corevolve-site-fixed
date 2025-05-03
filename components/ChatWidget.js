a0699ef534fb3b406b92b66e79cae2587635094f
Juan Manuel Mendoza <hojasdevida1978@gmail.com>
2025-05-02 23:44:55 -0400

    Added live chat widget via Tawk.to

---
+++ components/ChatWidget.js
@@ -0,0 +1,10 @@
+import Script from 'next/script';
+
+const ChatWidget = () => (
+  <Script
+    strategy="lazyOnload"
+    src="https://embed.tawk.to/68158d2ffc50e9190eb83df0/1iqa2v07a"
+  />
+);
+
+export default ChatWidget;
